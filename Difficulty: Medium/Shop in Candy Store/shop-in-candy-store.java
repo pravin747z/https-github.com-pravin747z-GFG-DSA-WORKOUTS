5dd59c27b07b1b255462d3cf25bc0efd3d0ddc70
Praveen Kumar <praveenkumar.m2004pk@gmail.com>
2025-08-12 07:14:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Shop in Candy Store/shop-in-candy-store.java
@@ -0,0 +1,26 @@
+class Solution {
+    public ArrayList<Integer> minMaxCandy(int[] prices, int k) {
+        Arrays.sort(prices);
+        ArrayList<Integer> lst=new ArrayList<>();
+        int Cost=0, pointer=0, len=prices.length;
+        int count=0;
+        
+        while(pointer<len && count<len){
+            Cost+=prices[pointer];
+            count+=1+k;
+            pointer++;
+        }
+        lst.add(Cost);
+        pointer=len-1;
+        Cost=0;
+        count=0;
+        
+        while(pointer>=0 &&count<len){
+            Cost+=prices[pointer];
+            count+=1+k;
+            pointer--;
+        }
+        lst.add(Cost);
+        return lst;
+    }
+}
